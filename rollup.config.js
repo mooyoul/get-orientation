61be2f027c22d70dce54bbd1152da9c0945606df
MooYeol Prescott Lee <mooyoul@gmail.com>
2021-06-21 09:13:34 +0900

    chore(deps): regular mainternance

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,32 @@
-import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript';
 
-export default {
+export default [{
   input: 'browser.ts',
   output: {
     format: 'umd',
-    file: 'browser.js',
+    dir: '.',
     name: 'getOrientation',
     sourcemap: true,
   },
   plugins: [
-    typescript({ tsconfig: "tsconfig.browser.json" }),
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    commonjs({ extensions: [".js", ".ts"]}),
+    typescript({ tsconfig: 'tsconfig.browser.json' }),
   ],
-};
+}, {
+  input: 'browser.es5.ts',
+  output: {
+    format: 'umd',
+    dir: '.',
+    name: 'getOrientation',
+    sourcemap: true,
+  },
+  plugins: [
+    nodeResolve({ browser: true, preferBuiltins: false }),
+    commonjs({ extensions: [".js", ".ts"]}),
+    typescript({ tsconfig: "tsconfig.browser.es5.json" }),
+  ],
+}];
+
